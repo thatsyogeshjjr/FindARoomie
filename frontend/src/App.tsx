4c0465dc7e3e63689861e6a5f573b3e9666906a2
Yogesh Jajoria <yogeshjajoria2019@gmail.com>
2024-09-07 19:46:51 +0530

    main page made

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,277 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // const blocks = [B1,B2,B3,B4,B5,B6,B,7]
+  const [block, setBlock] = useState("");
+  const [floor, setFloor] = useState(0);
+  const [room, setRoom] = useState(0);
+  const [pref, setPreference] = useState("");
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col items-center w-screen">
+      <div className="header w-fit m-2 mt-4 rounded-md p-5 bg-primary">
+        <h2 className="text-2xl text-white font-bold">Find a Roomie</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="h-6"></div>
+      <div className="shadow-md rounded-md px-6 py-4 flex flex-col gap-4">
+        <div>
+          <h3 className="text-xl">Find Rooms</h3>
+          <hr />
+        </div>
+        <select
+          name="block"
+          id=""
+          className="text-lg px-6 w-[60vw] sm:max-w-18 py-2 rounded-2xl bg-grey"
+        >
+          <option value="none" onClick={() => setBlock("b1")}>
+            Block
+          </option>
+          <option value="b1" onClick={() => setBlock("b1")}>
+            B1
+          </option>
+          <option value="b2" onClick={() => setBlock("b2")}>
+            B2
+          </option>
+          <option value="b3" onClick={() => setBlock("b3")}>
+            B3
+          </option>
+          <option value="b4" onClick={() => setBlock("b4")}>
+            B4
+          </option>
+          <option value="b5" onClick={() => setBlock("b5")}>
+            B5
+          </option>
+          <option value="b6" onClick={() => setBlock("b6")}>
+            B6
+          </option>
+          <option value="b7" onClick={() => setBlock("b7")}>
+            B7
+          </option>
+          <option value="b8" onClick={() => setBlock("b8")}>
+            B8
+          </option>
+          <option value="b9" onClick={() => setBlock("b9")}>
+            B9
+          </option>
+          <option value="b10" onClick={() => setBlock("b10")}>
+            B10
+          </option>
+          <option value="g1" onClick={() => setBlock("g1")}>
+            G1
+          </option>
+          <option value="g2" onClick={() => setBlock("g2")}>
+            G2
+          </option>
+          <option value="g3" onClick={() => setBlock("g3")}>
+            G3
+          </option>
+          <option value="g4" onClick={() => setBlock("g4")}>
+            G4
+          </option>
+          <option value="g5" onClick={() => setBlock("g5")}>
+            G5
+          </option>
+          <option value="g6" onClick={() => setBlock("g6")}>
+            G6
+          </option>
+          <option value="g7" onClick={() => setBlock("g7")}>
+            G7
+          </option>
+        </select>
+        <button
+          className="bg-dblue text-white rounded-3xl p-2 text-md w-fit px-8 py-2 "
+          onClick={() => {}}
+        >
+          Search
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className="shadow-md rounded-md px-6 py-4 flex flex-col gap-4">
+        <div>
+          <h3 className="text-xl">Add your room</h3>
+          <hr />
+        </div>
+        <select
+          name="block"
+          id=""
+          className="text-lg px-6 w-[60vw] sm:max-w-18 py-2 rounded-2xl bg-grey"
+        >
+          <option value="none" onClick={() => setBlock("b1")}>
+            Block
+          </option>
+          <option value="b1" onClick={() => setBlock("b1")}>
+            B1
+          </option>
+          <option value="b2" onClick={() => setBlock("b2")}>
+            B2
+          </option>
+          <option value="b3" onClick={() => setBlock("b3")}>
+            B3
+          </option>
+          <option value="b4" onClick={() => setBlock("b4")}>
+            B4
+          </option>
+          <option value="b5" onClick={() => setBlock("b5")}>
+            B5
+          </option>
+          <option value="b6" onClick={() => setBlock("b6")}>
+            B6
+          </option>
+          <option value="b7" onClick={() => setBlock("b7")}>
+            B7
+          </option>
+          <option value="b8" onClick={() => setBlock("b8")}>
+            B8
+          </option>
+          <option value="b9" onClick={() => setBlock("b9")}>
+            B9
+          </option>
+          <option value="b10" onClick={() => setBlock("b10")}>
+            B10
+          </option>
+          <option value="g1" onClick={() => setBlock("g1")}>
+            G1
+          </option>
+          <option value="g2" onClick={() => setBlock("g2")}>
+            G2
+          </option>
+          <option value="g3" onClick={() => setBlock("g3")}>
+            G3
+          </option>
+          <option value="g4" onClick={() => setBlock("g4")}>
+            G4
+          </option>
+          <option value="g5" onClick={() => setBlock("g5")}>
+            G5
+          </option>
+          <option value="g6" onClick={() => setBlock("g6")}>
+            G6
+          </option>
+          <option value="g7" onClick={() => setBlock("g7")}>
+            G7
+          </option>
+        </select>
+
+        <input
+          type="text"
+          placeholder="Floor"
+          className="outline-none border-dblue border-b-2"
+          onChange={(event) => setFloor(parseInt(event.target.value))}
+        />
+        <input
+          type="text"
+          placeholder="Room No."
+          className="outline-none border-dblue border-b-2"
+          onChange={(event) => setRoom(parseInt(event.target.value))}
+        />
+        <textarea
+          name=""
+          id=""
+          className="outline-none border-dblue border-b-2"
+          placeholder="Your contact + Any preferences ?"
+          onChange={(event) => setPreference(event.target.value)}
+        ></textarea>
+
+        <button
+          className="bg-dblue text-white rounded-3xl p-2 text-md w-fit px-8 py-2 "
+          onClick={() => {}}
+        >
+          Add
+        </button>
+      </div>
+      <div className="shadow-md rounded-md px-6 py-4 flex flex-col gap-4">
+        <div>
+          <h3 className="text-xl">Remove your Room</h3>
+          <hr />
+        </div>
+        <select
+          name="block"
+          id=""
+          className="text-lg px-6 w-[60vw] sm:max-w-18 py-2 rounded-2xl bg-grey"
+        >
+          <option value="none" onClick={() => setBlock("b1")}>
+            Block
+          </option>
+          <option value="b1" onClick={() => setBlock("b1")}>
+            B1
+          </option>
+          <option value="b2" onClick={() => setBlock("b2")}>
+            B2
+          </option>
+          <option value="b3" onClick={() => setBlock("b3")}>
+            B3
+          </option>
+          <option value="b4" onClick={() => setBlock("b4")}>
+            B4
+          </option>
+          <option value="b5" onClick={() => setBlock("b5")}>
+            B5
+          </option>
+          <option value="b6" onClick={() => setBlock("b6")}>
+            B6
+          </option>
+          <option value="b7" onClick={() => setBlock("b7")}>
+            B7
+          </option>
+          <option value="b8" onClick={() => setBlock("b8")}>
+            B8
+          </option>
+          <option value="b9" onClick={() => setBlock("b9")}>
+            B9
+          </option>
+          <option value="b10" onClick={() => setBlock("b10")}>
+            B10
+          </option>
+          <option value="g1" onClick={() => setBlock("g1")}>
+            G1
+          </option>
+          <option value="g2" onClick={() => setBlock("g2")}>
+            G2
+          </option>
+          <option value="g3" onClick={() => setBlock("g3")}>
+            G3
+          </option>
+          <option value="g4" onClick={() => setBlock("g4")}>
+            G4
+          </option>
+          <option value="g5" onClick={() => setBlock("g5")}>
+            G5
+          </option>
+          <option value="g6" onClick={() => setBlock("g6")}>
+            G6
+          </option>
+          <option value="g7" onClick={() => setBlock("g7")}>
+            G7
+          </option>
+        </select>
+
+        <input
+          type="text"
+          placeholder="Floor"
+          className="outline-none border-dblue border-b-2"
+          onChange={(event) => setFloor(parseInt(event.target.value))}
+        />
+        <input
+          type="text"
+          placeholder="Room No."
+          className="outline-none border-dblue border-b-2"
+          onChange={(event) => setRoom(parseInt(event.target.value))}
+        />
+
+        <button
+          className="bg-dblue text-white rounded-3xl p-2 text-md w-fit px-8 py-2 "
+          onClick={() => {}}
+        >
+          Get if off here
+        </button>
+      </div>
+      <div className="h-4"></div>
+      <p>This page is not affliated to GHS, it's a student project</p>
+      <p>For students, By students</p>
+      <div className="h-4"></div>
+      <p>Having Problems / love my work?</p>
+      <p>Contact me: yogeshjajoria2019@gmail.com</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: "#6096E0",
+      black: "#000",
+      white: "#fff",
+      dblue: "#08040f",
+      grey: "#DAE3E5",
+    },
   },
   plugins: [],
-}
-
+};
